a2e8131a78f75a1cf075ac40f2c060f4724e4eca
c11906941-hub <c11906941@gmail.com>
2025-12-23 22:24:56 -0500

    server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("NBA Proxy is running");
+});
+
 app.post("/fetch", async (req, res) => {
   try {
     const { url } = req.body;
